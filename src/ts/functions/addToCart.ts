cf5fd64134ba0172aa3e3f8ccc5a25a357c0833e
tuvaww <tuvali.wahlin@icloud.com>
2021-12-08 10:56:17 +0100

    .

---
+++ src/ts/functions/addToCart.ts
@@ -0,0 +1,14 @@
+/*import {productList} from "../models/objects"
+import {Products} from "../models/Products"
+function addToCart ({category} :Products ){
+    let cartSymbol = document.getElementsByClassName("fa-cart-plus");
+    
+    
+    for(let i = 0; i<cartSymbol.length; i ++){
+        let addButton = cartSymbol[i] as HTMLElement;
+        addButton.addEventListener("click", ()=>{
+
+        })
+    }
+
+}*/
