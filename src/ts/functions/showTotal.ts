154bdfa9517ecb37a9a2a2cf7413c3e314f27472
tuvaww <tuvali.wahlin@icloud.com>
2021-12-08 17:37:48 +0100

    more js element

--- src/ts/functions/showTotal.ts
+++ src/ts/functions/showTotal.ts
@@ -3,9 +3,11 @@ import { productList } from "../models/productList";
 export function showTotal() {
   let sum = 0;
   for (let i = 0; i < productList.length; i++) {
-    let price = productList[i].price;
-    sum += price;
-    console.log(sum);
+    if (productList[i].cart === true) {
+      let price = productList[i].price;
+      sum += price;
+      console.log(sum);
+    }
   }
   let totalSum = document.getElementsByClassName("addSum")[0];
   totalSum.innerHTML = "$" + sum;
