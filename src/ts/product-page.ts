1e53eba5ab532c766d0f39a7c2e094ce06c79f81
tuvaww <tuvali.wahlin@icloud.com>
2021-12-08 12:11:26 +0100

    .

--- src/ts/product-page.ts
+++ src/ts/product-page.ts
@@ -1,10 +1,8 @@
 import { burgerMenu } from "./functions/burgerMenu";
 import { createProductHtml } from "./functions/createProductHtml";
-
+import { addToCart } from "./functions/addToCart";
 
 window.onload = () => {
-    burgerMenu();
-    createProductHtml();
-
+  burgerMenu();
+  createProductHtml();
 };
-
