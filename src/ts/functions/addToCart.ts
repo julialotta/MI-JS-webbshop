5a7199b6160e9dd140aa391e02b9a5dfc86a3c36
tuvaww <tuvali.wahlin@icloud.com>
2021-12-10 16:22:27 +0100

    .

--- src/ts/functions/addToCart.ts
+++ src/ts/functions/addToCart.ts
@@ -4,10 +4,10 @@ import { productList } from "../models/productList";
 import { updateCartLocalStorage } from "./updateCartLocalStorage";
 
 export function addToCart(i: number) {
-    let newCart = productList[i];
-    let cart1 = new Orderinfo (newCart,1);
-    orderInfoList.push(cart1);
-    let p = document.getElementById("floatingcartnumber");
+  let newCart = productList[i];
+  let cart1 = new Orderinfo(newCart, 1);
+  orderInfoList.push(cart1);
+  let p = document.getElementById("floatingcartnumber");
   p.innerHTML = orderInfoList.length;
-    updateCartLocalStorage();
+  updateCartLocalStorage();
 }
